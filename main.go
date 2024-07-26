b06b295d645e080e8938edd8a46104a629c8417d
litongjava <litongjava@qq.com>
2024-07-26 19:39:30 +0900

    add support for single file

--- main.go
+++ main.go
@@ -18,24 +18,24 @@ import (
 )
 
 func main() {
-  // 定义命令行参数
+  // Define command line parameters
   zipFile := flag.String("r", "", "Archive zip file name")
   excludeFile := flag.String("x", "", "Exclude file pattern")
   flag.Parse()
 
-  // 检查是否有足够的参数
+  // Check if there are enough parameters
   if *zipFile == "" || flag.NArg() < 1 {
-    log.Fatalln("Usage: go-zip -r archive.zip directory")
+    log.Fatalln("Usage: go-zip -r archive.zip directory_or_file")
   }
 
-  // 获取额外的非flag参数，即目录名称
-  sourceDir := flag.Arg(0)
+  // Get extra non-flag parameters, i.e., directory or file name
+  sourcePath := flag.Arg(0)
 
-  // 输出解析得到的参数值，用于验证
-  log.Printf("Zip File: %s, Exclude Pattern: %s, Source Directory: %s\n", *zipFile, *excludeFile, sourceDir)
+  // Output the parsed parameter values for validation
+  log.Printf("Zip File: %s, Exclude Pattern: %s, Source Path: %s\n", *zipFile, *excludeFile, sourcePath)
 
-  // 设置压缩包名称和目录
-  err := Zip(sourceDir, zipFile, excludeFile)
+  // Set zip file name and directory
+  err := Zip(zipFile, sourcePath, excludeFile)
   if err != nil {
     fmt.Println(err)
   } else {
@@ -43,7 +43,7 @@ func main() {
   }
 }
 
-func Zip(sourceDir string, target *string, excludeFile *string) error {
+func Zip(target *string, sourcePath string, excludeFile *string) error {
   zipfile, err := os.Create(*target)
   if err != nil {
     return err
@@ -52,66 +52,87 @@ func Zip(sourceDir string, target *string, excludeFile *string) error {
 
   archive := zip.NewWriter(zipfile)
   defer archive.Close()
-  base := filepath.Base(sourceDir)
-  filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
-    if err != nil {
-      return err
-    }
 
-    // 获取相对路径
-    relPath, err := filepath.Rel(sourceDir, path)
-
-    if relPath == "." || (excludeFile != nil && matchExcludeFile(relPath, *excludeFile)) {
-      return nil // 跳过根目录或匹配排除模式的文件
-    }
-
-    header, err := zip.FileInfoHeader(info)
-    if err != nil {
-      return err
-    }
-    if err != nil {
-      return err
-    }
-    if relPath == "." {
-      return nil
-    } else if strings.Contains(relPath, string(os.PathSeparator)) {
-      relPath = strings.Replace(relPath, string(os.PathSeparator), "/", len(relPath))
-    }
-    //处理中文编码
-    if IsChineseChar(relPath) {
-      relPath = GetChineseName(relPath)
-    }
-
-    header.Name = base + "/" + relPath
-
-    if info.IsDir() {
-      header.Name += "/"
-    } else {
-      header.Method = zip.Deflate
-    }
-
-    writer, err := archive.CreateHeader(header)
-    if err != nil {
-      return err
-    }
-
-    if info.IsDir() {
-      return nil
-    }
-
-    file, err := os.Open(path)
-    if err != nil {
-      return err
-    }
-    defer file.Close()
-    _, err = io.Copy(writer, file)
+  info, err := os.Stat(sourcePath)
+  if err != nil {
     return err
-  })
+  }
+
+  var base string
+  if info.IsDir() {
+    base = filepath.Base(sourcePath)
+  } else {
+    base = filepath.Base(filepath.Dir(sourcePath))
+  }
+
+  if info.IsDir() {
+    err = filepath.Walk(sourcePath, func(path string, info os.FileInfo, err error) error {
+      return addFileToZip(archive, path, sourcePath, base, excludeFile, info)
+    })
+  } else {
+    err = addFileToZip(archive, sourcePath, filepath.Dir(sourcePath), base, excludeFile, info)
+  }
 
   return err
 }
 
-// matchExcludeFile 检查文件名是否匹配排除模式
+func addFileToZip(archive *zip.Writer, path, sourcePath, base string, excludeFile *string, info os.FileInfo) error {
+  if info.IsDir() && path == sourcePath {
+    return nil
+  }
+
+  // Get relative path
+  relPath, err := filepath.Rel(sourcePath, path)
+  if err != nil {
+    return err
+  }
+
+  if excludeFile != nil && matchExcludeFile(relPath, *excludeFile) {
+    return nil // Skip files matching the exclude pattern
+  }
+
+  header, err := zip.FileInfoHeader(info)
+  if err != nil {
+    return err
+  }
+
+  if strings.Contains(relPath, string(os.PathSeparator)) {
+    relPath = strings.Replace(relPath, string(os.PathSeparator), "/", -1)
+  }
+
+  // Handle Chinese encoding
+  if IsChineseChar(relPath) {
+    relPath = GetChineseName(relPath)
+  }
+
+  header.Name = base + "/" + relPath
+
+  if info.IsDir() {
+    header.Name += "/"
+  } else {
+    header.Method = zip.Deflate
+  }
+
+  writer, err := archive.CreateHeader(header)
+  if err != nil {
+    return err
+  }
+
+  if info.IsDir() {
+    return nil
+  }
+
+  file, err := os.Open(path)
+  if err != nil {
+    return err
+  }
+  defer file.Close()
+
+  _, err = io.Copy(writer, file)
+  return err
+}
+
+// matchExcludeFile checks if the filename matches the exclude pattern
 func matchExcludeFile(filename string, pattern string) bool {
   matched, err := filepath.Match(pattern, filename)
   if err != nil {
@@ -121,18 +142,18 @@ func matchExcludeFile(filename string, pattern string) bool {
   return matched
 }
 
-// 或者封装函数调用
+// IsChineseChar checks if the string contains Chinese characters
 func IsChineseChar(str string) bool {
   for _, r := range str {
     compile := regexp.MustCompile("[\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]")
-    if unicode.Is(unicode.Scripts["Han"], r) || (compile.MatchString(string(r))) {
+    if unicode.Is(unicode.Scripts["Han"], r) || compile.MatchString(string(r)) {
       return true
     }
   }
   return false
 }
 
-//对中文文件进行编码
+// GetChineseName encodes the filename in GB18030
 func GetChineseName(filename string) string {
   reader := bytes.NewReader([]byte(filename))
   encoder := transform.NewReader(reader, simplifiedchinese.GB18030.NewEncoder())
